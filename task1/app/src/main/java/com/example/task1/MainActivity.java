5376119942ce1521c8bec01281ad2098740ef093
usama7726 <80590352+usama7726@users.noreply.github.com>
2021-12-15 00:35:03 +0500

    Explicit Intent

--- task1/app/src/main/java/com/example/task1/MainActivity.java
+++ task1/app/src/main/java/com/example/task1/MainActivity.java
@@ -5,13 +5,18 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
+
+    override fun onCreate(Bundle savedInstanceState) {
+        val btn1: Button=findViewById(R.id.button5)
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.linearlayout);
+
     }
 
     @Override
@@ -20,4 +25,7 @@ public class MainActivity extends AppCompatActivity {
         inflater.inflate(R.menu.task1, menu);
         return true;
     }
+
+
+
 }
