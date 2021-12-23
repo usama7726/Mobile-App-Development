26ccc906cf08bb7fe8e8d665117b4c481ae10af1
usama7726 <80590352+usama7726@users.noreply.github.com>
2021-12-24 00:30:04 +0500

    task1 updated
    volley library implemented

--- task1/app/src/main/java/com/example/task1/MainActivity.kt
+++ task1/app/src/main/java/com/example/task1/MainActivity.kt
@@ -1,13 +1,22 @@
 package com.example.task1
 
+import android.app.Activity
+import android.app.DownloadManager
 import android.content.Intent
+import android.graphics.Bitmap
 import android.net.Uri
 import android.os.Bundle
 import android.os.PersistableBundle
+import android.util.Log
 import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
+import com.android.volley.Response
+import com.android.volley.toolbox.JsonObjectRequest
+import com.android.volley.toolbox.Volley
+
 
 class MainActivity: AppCompatActivity() {
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -18,10 +27,29 @@ class MainActivity: AppCompatActivity() {
 
         }
     }
-    fun viewContact(contactUri: Uri) {
+    fun viewContact() {
+        val contactUri = Uri.parse("www.google.com")
         val intent = Intent(Intent.ACTION_VIEW, contactUri)
         if (intent.resolveActivity(packageManager) != null) {
             startActivity(intent)
+            apicall()
         }
     }
+
+    private fun apicall() {
+        val Uri="https://some-random-api.ml/animal/dog"
+        val queue = Volley.newRequestQueue(this)
+        val jsonObjectRequest = JsonObjectRequest(
+            DownloadManager.Request.Method.Get,Uri, null,
+            Response.Listener{
+                Log.d("MainActivity", "Api call successful")
+
+            },Response.ErrorListener
+            {
+                Log.d("MainActivity", "Api call failed")
+            }
+        )
+        queue.add(JsonObjectRequest)
+    }
+
 }
